f70b5b3e1651d51d6c6959cad727a1ec47720007
hrt-lxg <liuxuegang@hrtpayment.com>
2019-04-18 17:45:58 +0800

    :tada: Init project

---
+++ src/main/java/com/github/xuegangliu/basictools/Main.java
@@ -0,0 +1,13 @@
+package com.github.xuegangliu.basictools;
+
+/**************************
+ * @description: Main
+ * @author: xuegangliu
+ * @date: 2019/4/18 17:34
+ * @since: jdk1.8+
+ ***************************/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello basicTools !!!");
+    }
+}
