e881a151a92617122985a9a069494cb363b0ecb0
hrt-lxg <liuxuegang@hrtpayment.com>
2019-04-19 11:17:07 +0800

    :pencil: Modify doc

--- src/main/java/com/github/xuegangliu/basictools/Main.java
+++ src/main/java/com/github/xuegangliu/basictools/Main.java
@@ -1,11 +1,10 @@
 package com.github.xuegangliu.basictools;
 
-/**************************
- * @description: Main
- * @author: xuegangliu
- * @date: 2019/4/18 17:34
- * @since: jdk1.8+
- ***************************/
+/**
+ * This is test
+ * @author      xuegangliu
+ * @since       JDK1.8+
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello basicTools !!!");
